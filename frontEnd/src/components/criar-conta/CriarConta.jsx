f8f70f04e7775b61d5471b4f163bc1e822783bc8
marcelo.muniz <marcelo_mbg@hotmail.com>
2023-02-28 09:06:41 -0300

    Colocando validações dos campos Nome, Sobrenome, Email, Senha e Confirmar Senha

--- frontEnd/src/components/criar-conta/CriarConta.jsx
+++ frontEnd/src/components/criar-conta/CriarConta.jsx
@@ -1,11 +1,11 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import OutlinedInput from '@mui/material/OutlinedInput';
-
-import { Link, useNavigate} from 'react-router-dom';
+import validator from 'email-validator';
+import { Link, useNavigate } from 'react-router-dom';
 
 import './styles.css'
 import { Footer } from '../footer/Footer';
@@ -14,130 +14,165 @@ export function CriarConta() {
 
 
   const [showPassword, setShowPassword] = React.useState(false);
-
-
   const handleClickShowPassword = () => setShowPassword((show) => !show);
-
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isConfirmSenhaEmpty, setIsConfirmSenhaEmpty] = useState(true);
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
 
-    const [userData, setUserData]  = useState({
-        nome: '',
-        sobreNome: '',
-        email: '',
-        senha: '',
-        
-}) 
-const navigate = useNavigate();
+  const [userData, setUserData] = useState({
+    nome: '',
+    sobreNome: '',
+    email: '',
+    senha: '',
 
-function handleChange(event) {
-  const {name, value } = event.target
-  setUserData(prevFormData => {
-      return {
-          ...prevFormData,
-          [name]: value
-      }
   })
-}
+  const navigate = useNavigate();
 
-async function handleSubmit(e){
-  e.preventDefault()
+  function handleChange(event) {
+    const { name, value } = event.target
+    setUserData(prevFormData => {
+      return {
+        ...prevFormData,
+        [name]: value
+      }
+    });
 
-  localStorage.setItem('data', JSON.stringify(userData))
-  
-  
-  alert('criar conta foi bem sucedido')
+    if (name === 'confirmSenha') {
+      setIsConfirmSenhaEmpty(value === '');
+    }
+  }
 
-  navigate('/iniciar-sessao')
-  
-};
+  function isValidName(value) {
+    return /^[a-zA-ZÀ-ÿ\u00f1\u00d1\s]+$/.test(value);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    // Validação do email
+    if (!validator.validate(userData.email)) {
+      alert('Email inválido!');
+      return;
+    }
+
+    // Validação do nome
+    if (!isValidName(userData.nome)) {
+      alert('Nome inválido!');
+      return;
+    }
+
+    // Validação do sobrenome
+    if (!isValidName(userData.sobreNome)) {
+      alert('Sobrenome inválido!');
+      return;
+    }
+
+    // Validação da senha
+    if (userData.senha.length < 6) {
+      alert('A senha deve ter pelo menos 6 caracteres!');
+      return;
+    }
+
+    // Verificar se as senhas coincidem
+    if (userData.senha !== userData.confirmSenha) {
+      setErrorMessage('As senhas não coincidem');
+      return;
+    }
+
+    localStorage.setItem('data', JSON.stringify(userData));
+    alert('A conta foi criada com sucesso!');
+    navigate('/iniciar-sessao');
+  };
 
   return (
     <>
-    <div className='iniciar-session-container'>
+      <div className='iniciar-session-container'>
         <h1 className='iniciar-title'>Criar conta</h1>
-        <form className='iniciar-form'  onSubmit={handleSubmit} >
-        <div className='iniciar-nome-sobreNome'>
-        <div className='nome-wrapper'>
-        <label htmlFor='name'>Nome: </label>
-        <input
-        className='input'
-          required
-          id="name"
-          size="small"
-          type = "text"
-          name='nome'
-          value={userData.nome}
-          onChange= {handleChange}
-        />
-        </div>
-        <div className='nome-wrapper'>
-        <label htmlFor='sobreNome'>Sobre nome: </label>
-        <input
-         className='input'
-          required
-          id="sobreNome"
-           name='sobreNome'
-           type="text"
-           value = {userData.sobreNome}
-           onChange= {handleChange}
-          size="small"
-        />
-       </div>
-        </div>
-        <label htmlFor='email'>Email: </label>
-        <input
-         className='input'
-          required
-          id="email"
-          type='email'
-          size="small"
-          name='email'
-          value={userData.email}
-          onChange= {handleChange}
-        />
-        <label htmlFor='senha'>Senha: </label>
-        <OutlinedInput
-         className='input'
-          required
-          id="senha"
-          size="small"
-          name="senha"
-          value = {userData.senha}
-          onChange= {handleChange}
-          type={showPassword ? 'text' : 'password'}
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-                edge="end"
-              >
-                {showPassword ?  <Visibility /> : <VisibilityOff />}
-              </IconButton>
-            </InputAdornment>
-          }
-        />
-         <label htmlFor='confirmSenha'>Confirm senha: </label>
-        <input
-          required
-          className='input'
-          id="confirmSenha"
-          size="small"
-          type='password'
-      
-        />
-        <span id="component-error-text" >Este campo é obrigatório</span>
-       <div className='btn-wrapper'>
-       <button className='iniciar-btn' type = 'submit'>Criar conta</button>
-      
-       <span className='iniciar-login'>Ja tem uma Conta? <Link className='login-link' to = "/iniciar-sessao">Iniciar sessão</Link></span>
-       </div>
+        <form className='iniciar-form' onSubmit={handleSubmit} >
+          <div className='iniciar-nome-sobreNome'>
+            <div className='nome-wrapper'>
+              <label htmlFor='name'>Nome: </label>
+              <input
+                className='input'
+                required
+                id="name"
+                size="small"
+                type="text"
+                name='nome'
+                value={userData.nome}
+                onChange={handleChange}
+              />
+            </div>
+            <div className='nome-wrapper'>
+              <label htmlFor='sobreNome'>Sobrenome: </label>
+              <input
+                className='input'
+                required
+                id="sobreNome"
+                name='sobreNome'
+                type="text"
+                value={userData.sobreNome}
+                onChange={handleChange}
+                size="small"
+              />
+            </div>
+          </div>
+          <label htmlFor='email'>Email: </label>
+          <input
+            className='input'
+            required
+            id="email"
+            type='email'
+            size="small"
+            name='email'
+            value={userData.email}
+            onChange={handleChange}
+          />
+          <label htmlFor='senha'>Senha: </label>
+          <OutlinedInput
+            className='input'
+            required
+            id="senha"
+            size="small"
+            name="senha"
+            value={userData.senha}
+            onChange={handleChange}
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+          <label htmlFor='confirmSenha'>Confirmar senha: </label>
+          <input
+            required
+            className='input'
+            id="confirmSenha"
+            size="small"
+            type='password'
+          />
+
+          {isConfirmSenhaEmpty === true && <span id="component-error-text" >Este campo é obrigatório</span>}
+          {errorMessage && <span id="component-error-text">{errorMessage}</span>}
+
+          <div className='btn-wrapper'>
+            <button className='iniciar-btn' type='submit'>Criar conta</button>
+
+            <span className='iniciar-login'>Ja tem uma Conta? <Link className='login-link' to="/iniciar-sessao">Iniciar sessão</Link></span>
+          </div>
         </form>
-    </div>
-    <Footer />
+      </div>
+      <Footer />
     </>
   )
 }
