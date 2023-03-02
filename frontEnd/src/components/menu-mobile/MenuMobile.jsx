8c8634419ea332f8a4b106ceb0d6b58a7b08f29c
Atef Chelaghma <atef.chelaghma@sovos.com>
2023-03-02 19:29:32 -0300

    update menu mobile

--- frontEnd/src/components/menu-mobile/MenuMobile.jsx
+++ frontEnd/src/components/menu-mobile/MenuMobile.jsx
@@ -6,46 +6,45 @@ import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import CssBaseline from '@mui/material/CssBaseline';
 import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import Button from '@mui/material/Button';
 import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import UserAvatar from '../user-avatar/UserAvatar';
+import { useLogin } from '../hooks/useLogin';
 import './styles.css'
 
 const drawerWidth = 240;
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginRight: -drawerWidth,
-    ...(open && {
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginRight: 0,
-    }),
-  }),
-);
+
+
+// const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+//   ({ theme, open }) => ({
+//     flexGrow: 1,
+//     padding: theme.spacing(3),
+//     transition: theme.transitions.create('margin', {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.leavingScreen,
+//     }),
+//     marginRight: -drawerWidth,
+//     ...(open && {
+//       transition: theme.transitions.create('margin', {
+//         easing: theme.transitions.easing.easeOut,
+//         duration: theme.transitions.duration.enteringScreen,
+//       }),
+//       marginRight: 0,
+//     }),
+//   }),
+// );
+
+
 
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== 'open',
@@ -74,9 +73,31 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 }));
 
 export default function PersistentDrawerRight() {
+
+  const { login } = useLogin();
+const location = useLocation();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
+  const renderCreateAccountButton = () => {
+    if (!login && location.pathname === '/criar-conta') {
+      return <ListItem  onClick={handleDrawerClose}><Link to="iniciar-sessao"><button className="login-btn">Iniciar sessão</button></Link></ListItem>;
+  
+    } else if (!login && location.pathname === '/iniciar-sessao') {
+      return <ListItem  onClick={handleDrawerClose}><Link to="criar-conta"><button className="login-btn">Criar conta</button></Link></ListItem>;
+  
+    } else if (!login) {
+      return <>
+        <ListItem  onClick={handleDrawerClose}><Link to="criar-conta"><button className="login-btn">Criar conta</button></Link></ListItem>
+        <ListItem  onClick={handleDrawerClose}><Link to="iniciar-sessao"><button className="login-btn">Iniciar sessão</button></Link></ListItem>
+      </>;
+    } else if (login) {
+       return <UserAvatar nome={localStorage.getItem('nome')} sobrenome={localStorage.getItem('sobreNome')} /> 
+    }
+    return null;
+  };
+
+  
   const handleDrawerOpen = () => {
     setOpen(true);
   };
@@ -126,7 +147,7 @@ export default function PersistentDrawerRight() {
           </IconButton>
         </DrawerHeader>
         <List sx = {{".MuiListItem-root" : {justifyContent: 'center'}}}>
-        <ListItem  onClick={handleDrawerClose}>
+        {/* <ListItem  onClick={handleDrawerClose}>
        
           <Link to= '/criar-conta'>
            <button className='menu-button'>
@@ -134,8 +155,8 @@ export default function PersistentDrawerRight() {
           </button>
           </Link>
        
-          </ListItem>
-        <ListItem onClick={handleDrawerClose} >
+          </ListItem> */}
+        {/* <ListItem onClick={handleDrawerClose} >
        
           <Link to= '/iniciar-sessao'>
            <button className='menu-button'>
@@ -143,23 +164,13 @@ export default function PersistentDrawerRight() {
           </button>
           </Link>
          
-          </ListItem>
+          </ListItem> */}
+
+          {renderCreateAccountButton()}
 
         </List>
         
         <Divider />
-        {/* <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List> */}
         <List className='footer-menu'>
           <InstagramIcon />
           <FacebookIcon />
