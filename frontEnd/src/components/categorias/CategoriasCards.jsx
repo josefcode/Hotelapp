4d806b18850a986c38be2745fa635c6886fd6023
marcelo.muniz <marcelo_mbg@hotmail.com>
2023-03-10 22:06:19 -0300

    Implementados consumos pelo Front das API de produtos, categorias, cidades

--- frontEnd/src/components/categorias/CategoriasCards.jsx
+++ frontEnd/src/components/categorias/CategoriasCards.jsx
@@ -5,22 +5,35 @@ import './styles.css'
 export  function CategoriasCards() {
 
    const [categoria, setCategoria] = React.useState([])
- 
+   const total = ['807.105 hotéis'] 
+
+   const requestConfig = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJUZXN0ZV9Vc2VybmFtZSIsImV4cCI6MTY3ODU4OTg4MSwiaWF0IjoxNjc4NDg5ODgxfQ.kj2TIbfoKKJjsAg_-NjZwgeHFoTV-wPqUAkbCr0L3OA"
+    }
+  };  
+
    React.useEffect(() => {
      async function fetchData(){
  
-        const response = await fetch(`http://localhost:3004/categorias`)
-       
+        const response = await fetch('http://localhost:8081/category/findAll', requestConfig)
+        
+
         if (!response.ok) {
          throw new Error(`HTTP error! status: ${response.status}`);
        }
         const data = await response.json()
        
         setCategoria(data)
+        console.log(data)
+
      }
  
      fetchData()
- 
+
+
    }, []);
 
   
@@ -29,8 +42,8 @@ export  function CategoriasCards() {
        <h2 className='title'> Buscar por tipo de acomodação</h2>
       <div className='cards-wrapper'>
         {
-            categoria.map(item => <CategoriaCard  key = {item.id} image = {item.image}  alt = {item.alt} totals={item.totals} 
-            title={item.name} />
+            categoria.map(item => <CategoriaCard  key = {item.id} image = {item.url_imagem}  alt = {item.alt} totals={total} 
+            title={item.descricao} />
                
             )
         }
--- frontEnd/src/components/recomendacoes/RecomendacoesCards.jsx
+++ frontEnd/src/components/recomendacoes/RecomendacoesCards.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import { RecomendacoeCard } from '../recomendacoe/RecomendacoeCard'
 import './styles.css'
 
-
-
-
 export  function RecomendacoesCards() {
-
   
   const [produto, setProduto] = React.useState([])
 
+  const requestConfig = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJUZXN0ZV9Vc2VybmFtZSIsImV4cCI6MTY3ODU4OTg4MSwiaWF0IjoxNjc4NDg5ODgxfQ.kj2TIbfoKKJjsAg_-NjZwgeHFoTV-wPqUAkbCr0L3OA"
+    }
+  };  
 
   React.useEffect(() => {
     async function fetchData(){
 
-       const response = await fetch(`http://localhost:3004/acomodacao`)
+       const response = await fetch(`http://localhost:8081/product/findAll`, requestConfig)
       
        if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
@@ -22,6 +25,7 @@ export  function RecomendacoesCards() {
        const data = await response.json()
       
       setProduto(data)
+      console.log(data)
     }
 
     fetchData()
@@ -33,18 +37,18 @@ export  function RecomendacoesCards() {
        <h2 className='recomendacaoes-title'> Recomendações </h2>
       <div className='recomendacaoes-cards-wrapper'>
         {
-            produto.map(item => <RecomendacoeCard  key = {item.id} 
-              id = {item.id}
-            image = {item.image}  
+            produto.map(item => <RecomendacoeCard  key = {item.id_produtos} 
+              id = {item.id_produtos}
+            image = {item.categoriasEntity.url_imagem}  
             alt = {item.alt} 
             type={item.type} 
-            title = {item.title} 
+            title = {item.nome} 
             puntaje = {item.puntaje} 
             distancia={item.distancia} 
             mapLink = {item.mapLink}
             comment = {item.comment}
             facilities = {item.facilities}
-            description = {item.description}
+            description = {item.descricao}
             verMais = {item.verMais} 
             stars = {item.stars}
             />
