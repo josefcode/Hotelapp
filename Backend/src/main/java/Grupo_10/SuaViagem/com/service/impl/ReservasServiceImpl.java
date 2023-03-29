62d3b40e3b5e40e3d89feb6abf5f81c1ccc28924
marcelo.muniz <marcelo_mbg@hotmail.com>
2023-03-29 12:07:05 -0300

    Incluído lógica para envio de email ao usuário que está logado

--- Backend/src/main/java/Grupo_10/SuaViagem/com/service/impl/ReservasServiceImpl.java
+++ Backend/src/main/java/Grupo_10/SuaViagem/com/service/impl/ReservasServiceImpl.java
@@ -6,14 +6,24 @@ import Grupo_10.SuaViagem.com.model.entity.ProdutosEntity;
 import Grupo_10.SuaViagem.com.model.entity.ReservasEntity;
 import Grupo_10.SuaViagem.com.repository.IProdutosRepository;
 import Grupo_10.SuaViagem.com.repository.IReservasRepository;
+import Grupo_10.SuaViagem.com.repository.IUserRepository;
 import Grupo_10.SuaViagem.com.service.IService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 
 @Service
 public class ReservasServiceImpl implements IService<ReservasDTO> {
@@ -23,6 +33,9 @@ public class ReservasServiceImpl implements IService<ReservasDTO> {
 
     @Autowired
     private IProdutosRepository iProdutosRepository;
+
+    @Autowired
+    private IUserRepository iUserRepository;
 
     @Override
     public ReservasDTO register(ReservasDTO reservasDTO) throws NotFoundException {
@@ -60,7 +73,88 @@ public class ReservasServiceImpl implements IService<ReservasDTO> {
         // Salva o objeto de produto atualizado no banco de dados
         iProdutosRepository.save(produtosEntity);
 
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String loggedUserEmail = authentication.getName();
+
+        // Configurações do servidor SMTP
+        String username = "noreplysuaviagem@gmail.com";
+        String password = "qglbxhdkymgzwjed";
+
+        // Configurações do e-mail
+        String from = "noreplysuaviagem@gmail.com";
+        String to = loggedUserEmail;
+        String subject = "SuaViagem.com";
+        String message = "Uma nova reserva foi feita.";
+
+        // Configura o servidor SMTP
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.socketFactory.port", "465");
+        properties.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.port", "465");
+
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        StringBuilder htmlBodyBuilder = new StringBuilder();
+        htmlBodyBuilder.append("<html>");
+        htmlBodyBuilder.append("<head>");
+        htmlBodyBuilder.append("<style>");
+//        htmlBodyBuilder.append("body { background-color: #ffffcc; }");
+//        htmlBodyBuilder.append("li { background-color: orange; font-size: 14px; }");
+        htmlBodyBuilder.append("</style>");
+        htmlBodyBuilder.append("</head>");
+        htmlBodyBuilder.append("<body>");
+        htmlBodyBuilder.append("<h1>Nova reserva</h1>");
+        htmlBodyBuilder.append("<p>Olá,</p>");
+        htmlBodyBuilder.append("<p>Uma nova reserva foi feita para o seguinte produto:</p>");
+        htmlBodyBuilder.append("<ul>");
+        htmlBodyBuilder.append("<li><strong>Nome do produto:</strong> " + produtosEntity.getNome() + "</li>");
+        htmlBodyBuilder.append("<li><strong>Data inicial:</strong> " + new SimpleDateFormat("dd-MM-yyyy").format(reservasDTO.getDataInicial()) + "</li>");
+        htmlBodyBuilder.append("<li><strong>Data final:</strong> " + new SimpleDateFormat("dd-MM-yyyy").format(reservasDTO.getDataFinal())  + "</li>");
+        htmlBodyBuilder.append("</ul>");
+        htmlBodyBuilder.append("<p>Atenciosamente,<br/>Equipe SuaViagem.com</p>");
+        htmlBodyBuilder.append("</body>");
+        htmlBodyBuilder.append("</html>");
+
+        String htmlBody = htmlBodyBuilder.toString();
+
+        try {
+            // Cria a mensagem de e-mail
+            MimeMessage mimeMessage = new MimeMessage(session);
+            mimeMessage.setFrom(new InternetAddress(from));
+            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            mimeMessage.setSubject(subject);
+
+            // Cria um objeto Multipart
+            Multipart multipart = new MimeMultipart();
+
+            // Adiciona o corpo em HTML à mensagem
+            BodyPart htmlPart = new MimeBodyPart();
+            htmlPart.setContent(htmlBody, "text/html; charset=utf-8");
+            multipart.addBodyPart(htmlPart);
+
+            // Define o Multipart como conteúdo da mensagem
+            mimeMessage.setContent(multipart);
+
+            // Envia a mensagem
+            Transport.send(mimeMessage);
+
+            System.out.println("Email de reserva enviado para " + loggedUserEmail);
+
+        } catch (MessagingException ex) {
+            ex.printStackTrace();
+        }
+
         return mapperEntityToDTO(reservasEntity);
+
+
     }
 
     @Override
