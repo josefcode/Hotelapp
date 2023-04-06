04388fee1fb53d987c5fde04daad5b3990edd771
unknown <atef.chela@gmail.com>
2023-04-06 15:45:26 -0700

    add menu bottao

--- frontEnd/src/components/user-avatar/UserAvatar.jsx
+++ frontEnd/src/components/user-avatar/UserAvatar.jsx
@@ -4,6 +4,13 @@ import { useLogin } from '../hooks/useLogin';
 import { useToken } from '../hooks/useToken';
 import CancelIcon from '@mui/icons-material/Cancel';
 import { useNavigate, Link} from 'react-router-dom';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import EditIcon from '@mui/icons-material/Edit';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import { styled, alpha } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 export default function UserAvatar() {
 
@@ -12,6 +19,16 @@ export default function UserAvatar() {
    const {changeLogin} = useLogin()
    const [userData, setUserData] = React.useState({})
    const [laoding, setLaoding] = React.useState(false)
+
+   const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
 
    const {token , changeToken} = useToken()
 
@@ -60,6 +77,47 @@ export default function UserAvatar() {
 
     }
 
+
+    const StyledMenu = styled((props) => (
+      <Menu
+        elevation={0}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        {...props}
+      />
+    ))(({ theme }) => ({
+      '& .MuiPaper-root': {
+        borderRadius: 6,
+        marginTop: theme.spacing(1),
+        minWidth: 180,
+        color:
+          theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+        boxShadow:
+          'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+        '& .MuiMenu-list': {
+          padding: '4px 0',
+        },
+        '& .MuiMenuItem-root': {
+          '& .MuiSvgIcon-root': {
+            fontSize: 18,
+            color: theme.palette.text.secondary,
+            marginRight: theme.spacing(1.5),
+          },
+          '&:active': {
+            backgroundColor: alpha(
+              theme.palette.primary.main,
+              theme.palette.action.selectedOpacity,
+            ),
+          },
+        },
+      },
+    }));
     
   return (
     <>
@@ -85,12 +143,47 @@ export default function UserAvatar() {
          {nome} {sobrenome}
          </p>
      </div>
+    <div>
+    <Button
+        id="demo-customized-button"
+        aria-controls={open ? 'demo-customized-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        variant="contained"
+        disableElevation
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon />}
+        size = 'small'
+        sx = {{
+          background: '#0F5EA2'
+        }}
+      >
+        Menu
+      </Button>
+    
+     <StyledMenu
+        id="demo-customized-menu"
+        MenuListProps={{
+          'aria-labelledby': 'demo-customized-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose} disableRipple style = {{ textDecoration: 'none', color: '#082442'}}>
+          <EditIcon />
+          Reservas 
+       
+        </MenuItem>
+        
+          
+         {userRoles === 'ROLE_ADMIN' &&<MenuItem onClick={handleClose} disableRipple> <Link style = {{ textDecoration: 'none', color: '#082442'}}to = 'administracao'><FileCopyIcon /> ADMIN</Link>  </MenuItem>}
+       
+      </StyledMenu>
+      </div>
+ 
     
     
-     <div>
-      
-     {userRoles === 'ROLE_ADMIN' && <Link to = 'administracao'><button  className='avata-btn'>ADM</button></Link>}
-     </div>
 
      <div className='avatar-close'>
      <CancelIcon onClick = {handleRemove} sx ={{fontSize: '20px',
