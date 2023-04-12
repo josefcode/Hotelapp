bd6927fc4d2775dea5c39abd30b03ca3feae3d28
RicardoGBrito <Ricardobrito_348@hotmail.com>
2023-04-11 21:17:00 -0300

    Finalização do histórico de reservas.

--- frontEnd/src/components/card-produto-reserva/cardProdutoReserva.jsx
+++ frontEnd/src/components/card-produto-reserva/cardProdutoReserva.jsx
@@ -15,21 +15,21 @@ export function CardProdutoReserva(props){
     
     return(
         <div className="card-produto-container">
-        <div className="card-produto-reserva">
-            <div >
-                <img className="card-produto-reserva-img" src={props.dados.img} />
-            </div>
-            <div className="card-produto-reserva-content">
-                <h3 className="card-produto-title">{props.dados.nome}</h3>
-                <div className="hours">
-                    <div style = {{background: '#0F5EA2', padding: '5px', borderRadius :'5px', color: '#EEF1F2'}}>
-                <p className="card-produto-chegada">Data de chegada: {ajustarData(props.dados.dataInicial)}</p>
-                <p className="card-produto-saida">Data de saída: {ajustarData(props.dados.dataFinal)}</p>
-                <p className="card-produto-entrada">Hora de entrada: {props.dados.horaEntrada}</p>
+            <div className="card-produto-reserva">
+                <div >
+                    <img className="card-produto-reserva-img" src={props.dados.img} />
+                </div>
+                <div className="card-produto-reserva-content">
+                    <h3 className="card-produto-title">{props.dados.nome}</h3>
+                    <div className="hours">
+                        <div style = {{background: '#0F5EA2', padding: '5px', borderRadius :'5px', color: '#EEF1F2'}}>
+                    <p className="card-produto-chegada">Data de chegada: {ajustarData(props.dados.dataInicial)}</p>
+                    <p className="card-produto-saida">Data de saída: {ajustarData(props.dados.dataFinal)}</p>
+                    <p className="card-produto-entrada">Hora de entrada: {props.dados.horaEntrada}</p>
+                    </div>
+                </div>
                 </div>
             </div>
-            </div>
-        </div>
         </div>
     )
 }
